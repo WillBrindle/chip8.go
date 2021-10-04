bbc1a4f8c478b0471793897ce93bc3e9d4876c46
willbrindle <will.brindle@slicelife.com>
2021-10-04 17:50:26 +0100

    Use ticker instead of hacky method

--- main.go
+++ main.go
@@ -11,19 +11,19 @@ import (
 func setup() {
 	display := pixeldisplay.New(8)
 	computer := chip8.New(display)
-	computer.LoadROM("pong.rom")
+	computer.LoadROM("roms/pong.rom")
+
+	ticker := time.NewTicker(time.Second / 300)
 
 	for !display.Closed() && !computer.IsHalted() {
-		st := time.Now()
 		computer.Tick()
 		display.Update(computer.GetScreen(), computer.GetDirtyFlags())
 
-		tt := time.Since(st)
-
-		time.Sleep((10 * time.Millisecond) - tt)
+		<-ticker.C
 	}
 
 	computer.Pause()
+	ticker.Stop()
 
 	// Keep the display running after halting; makes it easier to debug etc
 	for !display.Closed() {
